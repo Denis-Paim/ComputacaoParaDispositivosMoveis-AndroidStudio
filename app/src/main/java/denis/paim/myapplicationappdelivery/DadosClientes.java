71266245f1f9d523c43d4cf48a5d727387d8e97b
Denis-Paim <paimdenis@gmail.com>
2021-12-14 20:50:14 -0300

    CommitAppDelivery

---
+++ app/src/main/java/denis/paim/myapplicationappdelivery/DadosClientes.java
@@ -0,0 +1,71 @@
+package denis.paim.myapplicationappdelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DadosClientes extends AppCompatActivity {
+
+    private boolean switchLigado;
+
+    EditText edNomeCliente;
+    EditText edEnderecoCliente;
+    EditText edCidadeCliente;
+    EditText edCep;
+    EditText edNumeroCartao;
+    EditText edValidadeCartao;
+    EditText edNumeroSeguranca;
+    TextView txtFinalizarPedido;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dados_clientes);
+
+        edNomeCliente = findViewById(R.id.edNomeCliente);
+        edEnderecoCliente = findViewById(R.id.edEnderecoCliente);
+        edCidadeCliente = findViewById(R.id.edCidadeCliente);
+        edCep = findViewById(R.id.edCep);
+        edNumeroCartao = findViewById(R.id.edNumeroCartao);
+        edValidadeCartao = findViewById(R.id.edValidadeCartao);
+        edNumeroSeguranca = findViewById(R.id.edNumeroSeguranca);
+        txtFinalizarPedido = findViewById(R.id.txtFinalizarPedido);
+
+
+        Bundle bundle = getIntent().getExtras();
+        switchLigado = bundle.getBoolean("switchLigado");
+
+        if (switchLigado){
+
+            edEnderecoCliente.setVisibility(View.VISIBLE);
+            edCidadeCliente.setVisibility(View.VISIBLE);
+            edCep.setVisibility(View.VISIBLE);
+
+        }
+
+        txtFinalizarPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String cliente = edNomeCliente.getText().toString();
+                String cartao = edNumeroCartao.getText().toString();
+                String validadeCartao = edValidadeCartao.getText().toString();
+                String numeroSeguranca = edNumeroSeguranca.getText().toString();
+
+                if (cliente.isEmpty() || cartao.isEmpty() || validadeCartao.isEmpty() || numeroSeguranca.isEmpty()) {
+                    Toast.makeText(getApplicationContext(),
+                            getResources().getString(R.string.alerta),
+                            Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),
+                            getResources().getString(R.string.pedido_recebido)+" "+cliente,
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
